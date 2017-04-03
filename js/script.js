bcaa28f2398ce4b8b75a50fa1e8e8f00d4021fa1
Ignacio Fuentes <ignaciofuentes@outlook.com>
2017-04-03 16:11:52 -0400

    coutdown

--- js/script.js
+++ js/script.js
@@ -119,7 +119,7 @@
 
 		
 		/*CountDown*/
-		$('.married_coundown').countdown({until: new Date(2017, 6, 9)});
+		$('.married_coundown').countdown({until: new Date(2017, 6, 8)});
 		
 		/*OWL Carousel in Our Story*/
 		$(".story_wrapper").owlCarousel({
